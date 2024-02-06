61f5c29f5155c998077a5a3f07ba888b4d5420e8
PashaCD <104520780+PashaCD@users.noreply.github.com>
2024-02-06 20:27:26 +0200

    SideBar(smallfix), Header(90%)

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import Avatar from '../../assets/Avatar.png'
 
 export default function Header() {
 	return (
@@ -10,7 +11,9 @@ export default function Header() {
 			</label>
 			<button className='button_alert'></button>
 			<button className='button_message'></button>
-			<a href="" className='button_avatar'> </a>
+			<a href="" className='button_avatar' >
+				<img src={Avatar} alt="" />
+			</a>
 		</div>
 	)
 }
